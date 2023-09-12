8b6eace879fc573bfbc36125f31b11142ad85aa9
chemtrails <rafalprazmo@protonmail.com>
2023-09-12 19:33:48 +0200

    dump to github

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.contextMenus.onClicked.addListener(async (item, tab) => {
+    await chrome.tabs.sendMessage(tab.id, {id: item.menuItemId});
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'looop',
+        title: 'Looop',
+        contexts: ['page']
+    });
+})
---
+++ content.js
@@ -0,0 +1,130 @@
+const bar = document.createElement('div');
+const startBtn = document.createElement('div');
+const endBtn = document.createElement('div');
+const okBtn = document.createElement('div');
+const clearBtn = document.createElement('div');
+const wrapper1 = document.createElement('div');
+const wrapper2 = document.createElement('div');
+
+
+let target,
+    start,
+    end;
+
+chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.id !== 'looop') return;
+    bar.style.display = 'flex';
+});
+
+document.body.append(bar);
+bar.append(wrapper1);
+bar.append(wrapper2);
+wrapper1.append(startBtn);
+wrapper1.append(endBtn);
+wrapper2.append(okBtn);
+wrapper2.append(clearBtn);
+
+bar.id = 'looper-extension-bar';
+wrapper1.classList.add('looper-extension-wrapper')
+wrapper2.classList.add('looper-extension-wrapper')
+startBtn.classList.add('looper-extension-button');
+endBtn.classList.add('looper-extension-button');
+okBtn.classList.add('looper-extension-button');
+clearBtn.classList.add('looper-extension-button');
+
+startBtn.textContent = 'Loop start: 0';
+endBtn.textContent = 'Loop end: MAX';
+okBtn.textContent = 'Apply';
+clearBtn.textContent = 'Clear';
+
+clearBtn.onclick = () => {
+    if (target !== undefined) {
+        target.loop = false;
+        target.removeEventListener('timeupdate', onTime);
+
+        for (let medium of document.querySelectorAll(['video', 'audio'])) {
+            medium.removeEventListener('timeupdate', seekEnd);
+            medium.removeEventListener('timeupdate', seekStart);
+            medium.addEventListener('play', () => medium.loop = false);
+        }
+
+        target = undefined;
+    }
+
+    startBtn.textContent = 'Loop start: 0';
+    endBtn.textContent = 'Loop end: MAX';
+    endBtn.classList.remove('looper-extension-selected');
+    startBtn.classList.remove('looper-extension-selected');
+    bar.style.display = 'none';
+}
+
+startBtn.onclick = () => {
+    endBtn.classList.remove('looper-extension-selected');
+    startBtn.classList.add('looper-extension-selected');
+
+    if (target !== undefined) {
+        target.removeEventListener('timeupdate', seekEnd);
+        target.addEventListener('timeupdate', seekStart);
+        return;
+    }
+
+    for (let medium of document.querySelectorAll(['video', 'audio'])) {
+        medium.addEventListener('timeupdate', seekStart);
+    }
+}
+
+endBtn.onclick = () => {
+    endBtn.classList.add('looper-extension-selected');
+    startBtn.classList.remove('looper-extension-selected');
+
+    if (target !== undefined) {
+        target.removeEventListener('timeupdate', seekStart);
+        target.addEventListener('timeupdate', seekEnd);
+        return;
+    }
+
+    for (let medium of document.querySelectorAll(['video', 'audio'])) {
+        medium.addEventListener('timeupdate', seekEnd);
+    }
+}
+
+okBtn.onclick = () => {
+    endBtn.classList.remove('looper-extension-selected');
+    startBtn.classList.remove('looper-extension-selected');
+    bar.style.display = 'none';
+
+    if (target !== undefined) {
+        target.loop = true;
+
+        for (let medium of document.querySelectorAll(['video', 'audio'])) {
+            medium.removeEventListener('timeupdate', seekStart);
+            medium.removeEventListener('timeupdate', seekEnd);
+        }
+
+        target.addEventListener('timeupdate', onTime);
+    } else {
+        for (let medium of document.querySelectorAll(['video', 'audio'])) {
+            medium.addEventListener('play', medium.loop = true);
+        }
+    }
+}
+
+function seekStart(e) {
+    if (target === undefined) target = e.target;
+    start = target.currentTime;
+    startBtn.textContent = `Loop start: ${parseInt(start)}s`;
+}
+
+function seekEnd(e) {
+    if (target === undefined) target = e.target;
+    end = target.currentTime;
+    endBtn.textContent = `Loop end: ${parseInt(end)}s`;
+}
+
+function onTime() {
+    if (target === undefined) return;
+
+    if (target.currentTime < start || target.currentTime >= end) {
+        target.currentTime = start;
+    }
+}
